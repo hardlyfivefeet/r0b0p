4a31b9ea9967492342a4df0d65e58f19c6b5d876
Maddie Louis <mlouis2@lion.lmu.edu>
2020-03-05 15:15:31 -0800

    made it so you can print tree from command line like homework specification

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
+const fs = require("fs");
 const util = require("util");
+const yargs = require("yargs");
 const parse = require("./ast/parser");
 
-// const program = String.raw`SP3AK["Hello, world"];`;
-// const program = String.raw`PR3SUM1NG[x < 5] < >`;
-const program = String.raw`z = (-16.4 * 32) ** 8;`;
+// If compiling from a string, return the AST, IR, or compiled code as a string.
+function compile(sourceCode) {
+  let program = parse(sourceCode);
+  return util.inspect(program, { depth: null });
+}
 
-console.log(util.inspect(parse(program), { depth: null }));
+// If compiling from a file, write to standard output.
+function compileFile(filename, options) {
+  fs.readFile(filename, "utf-8", (error, sourceCode) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(compile(sourceCode, options));
+  });
+}
+
+// Two nice functions if you'd like to embed a compiler in your own apps.
+module.exports = { compile, compileFile };
+
+// Run the compiler as a command line application.
+if (require.main === module) {
+  const { argv } = yargs
+    .usage("$0 [-a] [-o] [-i] filename")
+    .boolean(["a", "o", "i"])
+    .describe("a", "show abstract syntax tree after parsing then stop")
+    .demand(1);
+  compileFile(argv._[0], {
+    astOnly: argv.a
+  });
+}
