d26ac62bc2e8131a598d5f20b5760657c01f343b
Maddie Louis <mlouis2@lion.lmu.edu>
2020-05-05 11:07:15 -0700

    remove stray comment

--- semantics/analyzer.js
+++ semantics/analyzer.js
@@ -169,7 +169,7 @@ KeyValue.prototype.analyze = function (context) {
   this.value.analyze(context);
 };
 
-Key.prototype.analyze = function (context) {};
+Key.prototype.analyze = function (context) { };
 
 List.prototype.analyze = function (context) {
   this.items.forEach((item) => {
@@ -223,13 +223,12 @@ Id.prototype.analyze = function (context) {
   const lookupResult = context.lookup(this);
   check.isNotUndeclaredVariable(lookupResult, this.name);
   lookupResult.referenced = true;
-  // this.value = this.name; // wait I dont think we need this anymore ??
 };
 
-BoolLit.prototype.analyze = function (context) {};
+BoolLit.prototype.analyze = function (context) { };
 
-IntLit.prototype.analyze = function (context) {};
+IntLit.prototype.analyze = function (context) { };
 
-FloatLit.prototype.analyze = function (context) {};
+FloatLit.prototype.analyze = function (context) { };
 
-Undefined.prototype.analyze = function (context) {};
+Undefined.prototype.analyze = function (context) { };
